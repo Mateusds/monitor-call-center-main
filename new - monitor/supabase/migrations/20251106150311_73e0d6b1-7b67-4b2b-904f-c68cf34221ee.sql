f73159d456461dedf44ceee9ae5c265b853a0251
Mateus Marques <mateusmarquesds@gmail.com>
2026-02-10 16:55:00 -0300

    remoção do new repositório

--- new - monitor/supabase/migrations/20251106150311_73e0d6b1-7b67-4b2b-904f-c68cf34221ee.sql
+++
@@ -1,5 +0,0 @@
--- Habilitar Realtime para a tabela call_records
-ALTER TABLE call_records REPLICA IDENTITY FULL;
-
--- Adicionar a tabela à publicação do realtime
-ALTER PUBLICATION supabase_realtime ADD TABLE call_records;
