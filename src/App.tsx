fb1cd7360bcc43076d22d992af1e35ff46b9a462
Mateus Marques <mateusmarquesds@gmail.com>
2026-02-11 11:23:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider delayDuration={300}>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Adicione o basename aqui */}
+      <BrowserRouter basename="/monitor-call-center-main">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
